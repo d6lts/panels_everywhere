f2789ab54573d7952cd05c455ab7cc1bb1ecc1b7
Earl Miles <merlin@logrus.com>
2010-11-03 00:12:56 +0000

    Fix header pane to at least not totally suck with Bartik.

--- theme/pane-header.tpl.php
+++ theme/pane-header.tpl.php
@@ -18,36 +18,37 @@
  * template_preprocess_pane_header() for examples.
  */
  ?>
-<div id="header clearfix">
-  <div id="logo-title">
+<div id="header">
+  <div class="section clearfix">
+    <div id="logo-title">
 
-    <?php if (!empty($logo)): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-    <?php endif; ?>
+      <?php if (!empty($logo)): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+        </a>
+      <?php endif; ?>
 
-    <div id="name-and-slogan">
-      <?php if ($site_name): ?>
-        <?php if ($title): ?>
-          <div id="site-name"><strong>
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </strong></div>
-        <?php else: /* Use h1 when the content title is empty */ ?>
-          <h1 id="site-name">
-            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
-          </h1>
+      <div id="name-and-slogan">
+        <?php if ($site_name): ?>
+          <?php if ($title): ?>
+            <div id="site-name"><strong>
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </strong></div>
+          <?php else: /* Use h1 when the content title is empty */ ?>
+            <h1 id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </h1>
+          <?php endif; ?>
         <?php endif; ?>
-      <?php endif; ?>
 
-      <?php if (!empty($site_slogan)): ?>
-        <div id="site-slogan"><?php print $site_slogan; ?></div>
-      <?php endif; ?>
-    </div> <!-- /name-and-slogan -->
-  </div> <!-- /logo-title -->
+        <?php if (!empty($site_slogan)): ?>
+          <div id="site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
+      </div> <!-- /name-and-slogan -->
+    </div> <!-- /logo-title -->
 
-  <?php if (!empty($search_box)): ?>
-    <div id="search-box"><?php print $search_box; ?></div>
-  <?php endif; ?>
-
+    <?php if (!empty($search_box)): ?>
+      <div id="search-box"><?php print $search_box; ?></div>
+    <?php endif; ?>
+  </div> <!-- /section -->
 </div> <!-- /header -->
